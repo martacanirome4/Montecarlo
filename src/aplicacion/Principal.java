0d4b6487122e3e2aa079008e5a0787a90d2935e9
MartaCanino <martacaninoromero@gmail.com>
2023-02-16 12:55:11 +0100

    Correcciones

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -3,14 +3,19 @@ import mates.Matematicas;
 import java.util.Scanner;
 
 public class Principal{
+    /**
+     * 
+     * @param args
+     */
     public static void main(String[] args) {
-        // Abrir el acceso a teclado para que el usuario introduzca el número de puntos a partir del que realizar el cálculo de pi
+
         Scanner keyboard = new Scanner(System.in);
+
         System.out.println("Introduzca una cantidad de puntos para realizar el cálculo:");
+
         int myint = keyboard.nextInt();
-        // Imprimir por pantalla el resultado
         System.out.println("El número Pi es " + Matematicas.generarNumeroPi(myint));
-        // Cerrar el acceso al teclado
+
         keyboard.close();
     }
 }
--- src/mates/Matematicas.java
+++ src/mates/Matematicas.java
@@ -2,35 +2,31 @@ package mates;
 import java.lang.Math;
 
 public class Matematicas {
+    /**
+     * 
+     * @param pasos
+     * @return
+     */
     public static double generarNumeroPi(long pasos) {
-        // método que permite obtener una aproximación al númpero pi mediante el método de Montecarlo y devuelva el número real que es una aproximación de pi
+
         int aciertos = 0;
-
         double pi;
-
-        double x = 0;
-        double y = 0;
-        double distancia = 0;
+        double x, y;
+        double distancia;
 
         for (int i=0; i<pasos; i++){
-            // 1. Generar dos números aleatorios entre -1 y 1
+
             x = Math.random() * 2 - 1;
             y = Math.random() * 2 - 1;
-            // 2. Calcular la distancia entre el punto (x,y) y el origen --> La raíz cuadrada de la suma de los cuadrados de los valores
-            // distancia = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+
             distancia = (Math.pow(x, 2) + Math.pow(y, 2));
-            // 3. Si la distancia es menor o igual que 1, el punto está dentro del círculo
+
             if (distancia < 1) {
                 aciertos++;
             }
-            // 4. Si la distancia es mayor que 1, el punto está fuera del círculo
-            // 5. Repetir el proceso tantas veces como indique el usuario
         }
-        // 6. Calcular el número de puntos que han caído dentro del círculo y dividirlo entre el número total de puntos
-        // 7. Multiplicar el resultado por 4
         pi = (double) 4.0*((float)aciertos / (float)pasos);
-        // 8. El resultado es una aproximación de pi
         return pi;
-
+    
     }
 }
