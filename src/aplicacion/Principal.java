23da57b2a148ae0323100230fa2fe11232519145
MartaCanino <martacaninoromero@gmail.com>
2023-02-16 11:44:58 +0100

    Completar comentarios

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -2,14 +2,15 @@ package aplicacion;
 import mates.Matematicas;
 import java.util.Scanner;
 
-// diagrama de clases + diagrama de flujo con plantUML
-
 public class Principal{
     public static void main(String[] args) {
+        // Acceso el acceso a teclado para que el usuario introduzca el número de puntos a partir del que realizar el cálculo de pi
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Introduzca una cantidad de puntos para realizar el cálculo:");
         int myint = keyboard.nextInt();
+        // Imprimir por pantalla el resultado
         System.out.println("El número Pi es " + Matematicas.generarNumeroPi(myint));
+        // Cerrar el acceso al teclado
         keyboard.close();
     }
 }
